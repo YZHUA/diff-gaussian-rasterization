0d5ed02a2b3c50b1ab035072fb8cfdf901a117ae
yzh <1347142721@qq.com>
2024-03-18 23:41:37 +0800

    Fix compile error on cuda 12

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ from setuptools import setup
 from torch.utils.cpp_extension import CUDAExtension, BuildExtension
 import os
 os.path.dirname(os.path.abspath(__file__))
-
+os.environ["TORCH_CUDA_ARCH_LIST"]= "5.0;6.0;6.1;6.2;7.0;7.5"
 setup(
     name="diff_gaussian_rasterization",
     packages=['diff_gaussian_rasterization'],
